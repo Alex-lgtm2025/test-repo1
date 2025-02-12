f7797e0e4b45bfca2906ac63aa369f32187da84e
Alex-lgtm2025 <au167186@gmail.com>
2025-02-12 17:03:37 +0300

    Добавление определение функции в задании 1.1

--- 1.1.py
+++ 1.1.py
@@ -4,6 +4,6 @@ a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
 p = (a + b + c) / 2
-s = treu(p * (p - a) * (p - b) * (p - c))
+s = (p * (p - a) * (p - b) * (p - c))**0.5
 print("Площадь: %.2f" % s)
  
