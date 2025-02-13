3741feec07feda652a0326486ecd6619d93f2d25
Alex-lgtm2025 <au167186@gmail.com>
2025-02-13 21:11:12 +0300

    Функция register

---
+++ 2.2.1.py
@@ -0,0 +1,11 @@
+name = str(input("Enter Your Name : "))
+surname = str(input("Enter Your Surname : "))
+email = str(input("Enter Your Email : "))
+password = str(input("Enter Your Password : "))
+confPassword = str(input("Confirm Your Password : "))
+  
+if(password == confPassword):
+    print("User Accepted!")
+    user = createUser(name, surname, email, password)
+else:
+      print("User Rejected! Invalid Password Combination")
